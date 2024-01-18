b01ceccdf321da9bb72dc9bd5f664138e6abea78
wbrian2016 <76930352+wbrian2016@users.noreply.github.com>
2024-01-17 18:37:29 -0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+                    {'SKU': '1044185', 'Description':'LUCKY LAGER 355ML 3X8UC CAN CARTON', 'Length':'100', 'Width':'80', 'Height':'40' },
+            {'SKU': '1044185', 'Description':'LUCKY LAGER 355ML 3X8UC CAN CARTON', 'Length':'100', 'Width':'80', 'Height':'40' },
+            {'SKU': '2044186', 'Description':'LUCKY LAGER 355ML 3X8UC CAN CARTON', 'Length':'100', 'Width':'80', 'Height':'40' },
+            {'SKU': '3044187', 'Description':'LUCKY LAGER 355ML 3X8UC CAN CARTON', 'Length':'100', 'Width':'80', 'Height':'40' },
+            {'SKU': '4044185', 'Description':'LUCKY LAGER 355ML 3X8UC CAN CARTON', 'Length':'100', 'Width':'80', 'Height':'40' }
+                ]
